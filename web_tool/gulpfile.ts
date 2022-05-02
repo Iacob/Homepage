f1269611b13507ac64dbdc68905e6356cb12414a
luo.yong <luoyong@jdyrobot.com>
2022-05-03 01:48:57 +0800

    加入对HAML文件的处理

--- web_tool/gulpfile.ts
+++ web_tool/gulpfile.ts
@@ -1,8 +1,9 @@
 
-import {mkdirSync} from 'fs';
+import { mkdirSync } from 'fs';
 
 var gulp:any = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+var haml = require('gulp-haml');
 
 function buildStyles() {
   return gulp.src('./src/**/*.sass')
@@ -10,15 +11,31 @@ function buildStyles() {
     .pipe(gulp.dest('./build'));
 };
 
+function buildHaml() {
+  return gulp.src('./src/**/*.haml')
+    .pipe(haml())
+    .pipe(gulp.dest('./build'));
+}
+
 function copyHtml() {
   return gulp.src(['./src/**/*.html', './src/**/*.js'])
     .pipe(gulp.dest('./build'));
 };
 
+function copyRes() {
+  return gulp.src(['./src/**/res/*'])
+    .pipe(gulp.dest('./build'));
+};
+
 exports.watchFiles = function () {
   mkdirSync('build', {recursive: true});
+  buildStyles();
+  buildHaml();
+  //copyHtml();
+  copyRes();
   gulp.watch(['./src/**/*.scss', './src/**/*.sass'], gulp.series('buildStyles'));
-  gulp.watch(['./src/**/*.html'], gulp.series('copyHtml'));
+  //gulp.watch(['./src/**/*.html'], gulp.series('copyHtml'));
+  gulp.watch(['./src/**/*.haml'], gulp.series('buildHaml'));
 };
 
 function defaultTask(cb:any) {
@@ -28,5 +45,7 @@ function defaultTask(cb:any) {
 }
 
 exports.buildStyles = buildStyles;
+exports.buildHaml = buildHaml;
 exports.copyHtml = copyHtml;
+exports.copyRes = copyRes;
 exports.default = defaultTask;
